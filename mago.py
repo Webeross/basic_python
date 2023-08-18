87ab5566b50c29436dae0628ade5cdf82affbf60
Eduardo Weber <99500922+Webeross@users.noreply.github.com>
2023-08-18 18:26:34 -0300

    Add files via upload

---
+++ mago.py
@@ -0,0 +1,21 @@
+secret_number = 777
+
+print(
+"""
++===================================+
+| Bem vindo ao meu jogo, trouxa!    |
+| Insira um número inteiro          |
+| e adivinhar o número que tenho    |
+| escolhidos para você.             |
+| Então, qual é o número secreto?   |
++===================================+
+""")
+
+numero = int(input('Tente a sorte trouxa!: '))
+
+
+
+while numero != 666:
+    print('"Ha ha! Você está preso no meu loop!"')
+
+print("Muito bem, trouxa! Você está livre agora.")
