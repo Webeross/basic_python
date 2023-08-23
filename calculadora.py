5933157dcad92952584c940d4d6cce6abfd87650
Eduardo Weber <99500922+Webeross@users.noreply.github.com>
2023-08-23 19:38:49 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+numero1 = float(input('Digite o primeiro número: '))
+
+numero2 = float(input('Digite o segundo número: '))
+
+operacao = input('Digite a operação (+, -, *, /): ')
+
+if operacao == '+':
+    print('Sua conta é: ', numero1 + numero2)
+
+elif operacao == '-':
+    print('Sua conta é', numero1 + numero2 )
+
+elif operacao == '*':
+    print('Sua conta é ', numero1 + numero2)
+
+else:
+    print('Sua conta é', numero1 / numero2 )
+    
